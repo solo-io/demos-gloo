d27573039598193e9df26df0f44d02abca813669
Scott Cranton <scott.cranton@solo.io>
2019-09-26 17:53:29 -0400

    bumped version

--- start_cluster.sh
+++ start_cluster.sh
@@ -24,7 +24,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$SCRIPT_DIR/working_environment.sh"
 
 GLOO_ENT_VERSION=0.18.32
-GLOO_OSS_VERSION=0.19.1
+GLOO_OSS_VERSION=0.19.2
 
 K8S_TOOL=${K8S_TOOL:-kind} # kind, minikube, minishift, gcloud
 
