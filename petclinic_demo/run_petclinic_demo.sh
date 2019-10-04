afdd67a77604a3556066be85f18f1a9e885bef3c
Scott Cranton <scott.cranton@solo.io>
2019-10-04 12:43:52 -0400

    cleanup

--- petclinic_demo/run_petclinic_demo.sh
+++ petclinic_demo/run_petclinic_demo.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+PROXY_PORT=9080
+WEB_UI_PORT=9088
+
+
 # Will exit script if we would use an uninitialised variable:
 set -o nounset
 # Will exit script when a simple command (not a control structure) fails:
@@ -22,31 +26,19 @@ if [[ $K8S_TOOL == 'kind' ]]; then
   export KUBECONFIG
 fi
 
+# Cleanup previous example runs
+kubectl --namespace='gloo-system' delete \
+  virtualservice/default && true # ignore errors
+
+#
+# Install example services and external upstreams
+#
+
 # Install petclinic application
 kubectl --namespace='default' apply \
   --filename="$GLOO_DEMO_RESOURCES_HOME/petclinic-db.yaml" \
   --filename="$GLOO_DEMO_RESOURCES_HOME/petclinic.yaml" \
   --filename="$GLOO_DEMO_RESOURCES_HOME/petclinic-vets.yaml"
-
-kubectl apply --filename - <<EOF
-apiVersion: gateway.solo.io/v1
-kind: VirtualService
-metadata:
-  name: default
-  namespace: gloo-system
-spec:
-  virtualHost:
-    domains:
-    - '*'
-    routes:
-    - matcher:
-        prefix: /
-      routeAction:
-        single:
-          upstream:
-            name: default-petclinic-8080
-            namespace: gloo-system
-EOF
 
 # Install petstore app to show OpenAPI
 kubectl --namespace='default' apply \
@@ -76,29 +68,65 @@ spec:
 EOF
 fi
 
+#
+# Configure Traffic Routing rules
+#
+
+# glooctl create virtualservice \
+#   --name='default' \
+#   --namespace='gloo-system'
+
+# glooctl add route \
+#   --name='default' \
+#   --path-prefix='/' \
+#   --dest-name='default-petclinic-8080' \
+#   --dest-namespace='gloo-system'
+
+kubectl apply --filename - <<EOF
+apiVersion: gateway.solo.io/v1
+kind: VirtualService
+metadata:
+  name: default
+  namespace: gloo-system
+spec:
+  virtualHost:
+    domains:
+    - '*'
+    routes:
+    - matcher:
+        prefix: /
+      routeAction:
+        single:
+          upstream:
+            name: default-petclinic-8080
+            namespace: gloo-system
+EOF
+
+#
+# Enable localhost access to cluster and open web brower clients
+#
+
 # Expose and open in browser GlooE Web UI Console
-WEB_UI_PORT=9088
-API_SERVER_PID_FILE=$SCRIPT_DIR/api_server_pf.pid
+API_SERVER_PID_FILE="$SCRIPT_DIR/api_server_pf.pid"
 if [[ -f $API_SERVER_PID_FILE ]]; then
   xargs kill <"$API_SERVER_PID_FILE" && true # ignore errors
   rm "$API_SERVER_PID_FILE"
 fi
-kubectl --namespace='gloo-system' rollout status deployment/api-server --watch=true
+kubectl --namespace='gloo-system' rollout status deployment/api-server --watch='true'
 ( (kubectl --namespace='gloo-system' port-forward deployment/api-server ${WEB_UI_PORT:-9088}:8080 >/dev/null) & echo $! > "$API_SERVER_PID_FILE" & )
 
 open "http://localhost:${WEB_UI_PORT:-9088}/"
 
 # Open in browser petclinic home page
-PROXY_PORT=9080
-PROXY_PID_FILE=$SCRIPT_DIR/proxy_pf.pid
+PROXY_PID_FILE="$SCRIPT_DIR/proxy_pf.pid"
 if [[ -f $PROXY_PID_FILE ]]; then
   xargs kill <"$PROXY_PID_FILE" && true # ignore errors
   rm "$PROXY_PID_FILE"
 fi
-kubectl --namespace='gloo-system' rollout status deployment/gateway-proxy-v2 --watch=true
-( (kubectl --namespace='gloo-system' port-forward service/gateway-proxy-v2 ${PROXY_PORT:-9080}:80 >/dev/null) & echo $! > "$PROXY_PID_FILE" & )
+kubectl --namespace='gloo-system' rollout status deployment/gateway-proxy-v2 --watch='true'
+( (kubectl --namespace='gloo-system' port-forward deployment/gateway-proxy-v2 ${PROXY_PORT:-9080}:8080 >/dev/null) & echo $! > "$PROXY_PID_FILE" & )
 
 # Wait for app to be fully deployed and running
-kubectl --namespace='default' rollout status deployment/petclinic --watch=true
+kubectl --namespace='default' rollout status deployment/petclinic --watch='true'
 
 open "http://localhost:${PROXY_PORT:-9080}/"
