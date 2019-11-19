9e91a6cbfc52c3a3342d6adff041c40c2c1b8a03
Scott Cranton <scott.cranton@solo.io>
2019-11-19 08:11:14 -0500

    polish

--- working_environment.sh
+++ working_environment.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-K8S_TOOL=minikube   # kind, minikube, minishift, gcloud, custom
-TILLER_MODE=cluster # local, cluster, none
-GLOO_MODE=ent       # oss, ent, knative, none
+K8S_TOOL='minikube'   # kind, minikube, minishift, gcloud, custom
+TILLER_MODE='cluster' # local, cluster, none
+GLOO_MODE='ent'       # oss, ent, knative, none
 
-GLOO_VERSION=0.21.0 # ent
+GLOO_VERSION='0.21.0' # ent
 
 GLOO_DEMO_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
